f3d20523ee6bf53904e10092f45f8ac1960ac7b6
BRAD <kbrad4056@yahoo.com>
2025-11-20 12:41:54 -0500

    Refine SQL query for top-paying data analyst jobs
    Updated SQL query to include company names and refined comments for clarity.

--- PROJECT_SQL/1_top_paying_jobs.sql
+++ PROJECT_SQL/1_top_paying_jobs.sql
@@ -1,15 +1,24 @@
 /*
-Question: what are the top paying data analysis jobs?
--Identify the top 10 paying data anlasyt roles that are available remotely.
--focus on the job postings with specified salary information(remove those with "not specified" salary info).
-why? highlight the top paying opptunities for data analyst offering insight into employment trends and salary expectations in the field.
-*/  
+Question: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely
+- Focuses on job postings with specified salaries (remove nulls)
+- BONUS: Include company names of top 10 roles
+- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment options and location flexibility.
+*/
 
 SELECT
+dim.name as company_name,
 job_id,
 job_title,
 job_location,
 job_schedule_type,
 salary_year_avg,
 job_posted_date
-from job_postings_facts 
+from job_postings_fact
+left JOIN company_dim as dim on dim.company_id = job_postings_fact.company_id
+WHERE  job_title_short ilike '%data analyst%'
+AND job_location = 'Anywhere'
+and salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+limit 10
+
