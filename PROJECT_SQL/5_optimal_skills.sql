bd4666756683f4511136b4ae70570fafffc5afb4
BRAD <kbrad4056@yahoo.com>
2025-11-25 10:53:27 -0500

    Add JSON data for skill demand and salaries

--- PROJECT_SQL/5_optimal_skills.sql
+++ PROJECT_SQL/5_optimal_skills.sql
@@ -103,3 +103,155 @@ data visualization tools (Tableau, Power BI),
 and cloud/data engineering tools (Snowflake, Azure, AWS)
 provide the strongest mix of job security + high salary.
 */
+[
+  {
+    "skill_id": 0,
+    "skill_name": "sql",
+    "demand_count": "525",
+    "avg_salary": "101923"
+  },
+  {
+    "skill_id": 182,
+    "skill_name": "tableau",
+    "demand_count": "310",
+    "avg_salary": "103785"
+  },
+  {
+    "skill_id": 1,
+    "skill_name": "python",
+    "demand_count": "308",
+    "avg_salary": "106138"
+  },
+  {
+    "skill_id": 181,
+    "skill_name": "excel",
+    "demand_count": "304",
+    "avg_salary": "89986"
+  },
+  {
+    "skill_id": 5,
+    "skill_name": "r",
+    "demand_count": "189",
+    "avg_salary": "104267"
+  },
+  {
+    "skill_id": 183,
+    "skill_name": "power bi",
+    "demand_count": "141",
+    "avg_salary": "100458"
+  },
+  {
+    "skill_id": 7,
+    "skill_name": "sas",
+    "demand_count": "82",
+    "avg_salary": "102204"
+  },
+  {
+    "skill_id": 186,
+    "skill_name": "sas",
+    "demand_count": "82",
+    "avg_salary": "102204"
+  },
+  {
+    "skill_id": 185,
+    "skill_name": "looker",
+    "demand_count": "81",
+    "avg_salary": "111849"
+  },
+  {
+    "skill_id": 196,
+    "skill_name": "powerpoint",
+    "demand_count": "72",
+    "avg_salary": "90284"
+  },
+  {
+    "skill_id": 80,
+    "skill_name": "snowflake",
+    "demand_count": "63",
+    "avg_salary": "115615"
+  },
+  {
+    "skill_id": 188,
+    "skill_name": "word",
+    "demand_count": "60",
+    "avg_salary": "86163"
+  },
+  {
+    "skill_id": 79,
+    "skill_name": "oracle",
+    "demand_count": "47",
+    "avg_salary": "108575"
+  },
+  {
+    "skill_id": 61,
+    "skill_name": "sql server",
+    "demand_count": "45",
+    "avg_salary": "102785"
+  },
+  {
+    "skill_id": 215,
+    "skill_name": "flow",
+    "demand_count": "42",
+    "avg_salary": "100967"
+  },
+  {
+    "skill_id": 74,
+    "skill_name": "azure",
+    "demand_count": "41",
+    "avg_salary": "113543"
+  },
+  {
+    "skill_id": 8,
+    "skill_name": "go",
+    "demand_count": "40",
+    "avg_salary": "118777"
+  },
+  {
+    "skill_id": 76,
+    "skill_name": "aws",
+    "demand_count": "38",
+    "avg_salary": "112958"
+  },
+  {
+    "skill_id": 192,
+    "skill_name": "sheets",
+    "demand_count": "35",
+    "avg_salary": "88650"
+  },
+  {
+    "skill_id": 199,
+    "skill_name": "spss",
+    "demand_count": "32",
+    "avg_salary": "100586"
+  },
+  {
+    "skill_id": 22,
+    "skill_name": "vba",
+    "demand_count": "32",
+    "avg_salary": "93759"
+  },
+  {
+    "skill_id": 233,
+    "skill_name": "jira",
+    "demand_count": "30",
+    "avg_salary": "106739"
+  },
+  {
+    "skill_id": 201,
+    "skill_name": "alteryx",
+    "demand_count": "28",
+    "avg_salary": "99532"
+  },
+  {
+    "skill_id": 97,
+    "skill_name": "hadoop",
+    "demand_count": "26",
+    "avg_salary": "115471"
+  },
+  {
+    "skill_id": 9,
+    "skill_name": "javascript",
+    "demand_count": "23",
+    "avg_salary": "101663"
+  }
+]
